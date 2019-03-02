9643df8dae3cc4905b50177c74ed7e849f7c9cfb
LekoArts <lekoarts@gmail.com>
2019-03-02 20:55:05 +0100

    add types

---
+++ src/types/gatsby.ts
@@ -0,0 +1,27 @@
+export interface GatsbyNode {
+  id: string // Gatsby node ID
+  parent?: string | null
+  children?: string[]
+  internal?: {
+    mediaType?: string
+    type: string
+    contentDigest: string
+  }
+  [key: string]: any
+}
+
+export type GatsbyNodeCreator = (node: GatsbyNode) => null
+
+export type GatsbyNodeIdCreator = (id: string, namespace?: string) => string
+
+export type GatsbyContentDigester = (content: string) => string
+
+export interface GatsbyContext {
+  actions: GatsbyActions
+  createNodeId: GatsbyNodeIdCreator
+  createContentDigest: GatsbyContentDigester
+}
+
+export interface GatsbyActions {
+  createNode: GatsbyNodeCreator
+}
