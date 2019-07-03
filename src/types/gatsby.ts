932f8ea3850835ba81a7143ec44dd0d24803b7bd
LekoArts <lekoarts@gmail.com>
2019-07-03 09:11:03 +0200

    chore: update dependencies

--- src/types/gatsby.ts
+++ src/types/gatsby.ts
@@ -1,27 +1,27 @@
 export interface GatsbyNode {
-  id: string // Gatsby node ID
-  parent?: string | null
-  children?: string[]
+  id: string; // Gatsby node ID
+  parent?: string | null;
+  children?: string[];
   internal?: {
-    mediaType?: string
-    type: string
-    contentDigest: string
-  }
-  [key: string]: any
+    mediaType?: string;
+    type: string;
+    contentDigest: string;
+  };
+  [key: string]: any;
 }
 
-export type GatsbyNodeCreator = (node: GatsbyNode) => null
+export type GatsbyNodeCreator = (node: GatsbyNode) => null;
 
-export type GatsbyNodeIdCreator = (id: string, namespace?: string) => string
+export type GatsbyNodeIdCreator = (id: string, namespace?: string) => string;
 
-export type GatsbyContentDigester = (content: string) => string
+export type GatsbyContentDigester = (content: string) => string;
 
 export interface GatsbyContext {
-  actions: GatsbyActions
-  createNodeId: GatsbyNodeIdCreator
-  createContentDigest: GatsbyContentDigester
+  actions: GatsbyActions;
+  createNodeId: GatsbyNodeIdCreator;
+  createContentDigest: GatsbyContentDigester;
 }
 
 export interface GatsbyActions {
-  createNode: GatsbyNodeCreator
+  createNode: GatsbyNodeCreator;
 }
